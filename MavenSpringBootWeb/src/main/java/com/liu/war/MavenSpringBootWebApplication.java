373ae15a6dceb44c86ae1d7d2b7443c64d57853a
123610420@qq.com <123610420@qq.com>
2019-09-01 00:09:28 +0800

    one

---
+++ MavenSpringBootWeb/src/main/java/com/liu/war/MavenSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.liu.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpringBootWebApplication.class, args);
+	}
+
+}
